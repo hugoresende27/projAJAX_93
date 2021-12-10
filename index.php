9e562fafba5390386eb6897559cd5269f80e971b
hugoresende27 <Hugoresende27@gmail.com>
2021-12-10 09:30:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,18 @@
 
 <h3>Clientes</h3>
 <hr>
-<div id="listaClientes">
-    <ul>
-        <li></li>
-    </ul>
-</div>
+    <div id="detailsCliente">
+        <small>Não existe cliente seleccionado</small>
+    </div>
+
+<hr>
+    <div id="listaClientes">
+        
+        <!-- <ul>
+            <li></li>
+        </ul> -->
+    </div>
+<button onclick="listaCli()">Atualizar</button>
 
 <script>
 
@@ -34,19 +41,36 @@ $(document).ready(function(){
             success: function(dados){
                 console.log("OK")
                 var clientes = JSON.parse(dados);//coleção de dados
+                /*
                 clientes.forEach(ele => {
                     console.log(ele['nome'])
                     $('#listaClientes').text(ele['nome']);
                 })
+                */
                 
                 //$('#listaClientes').text(clientes[0]['nome']);
+
+                //IMPRIMIR LISTA COMPLETA DOS CLIENTES
+                var html = "<ul>";
+                clientes.forEach ( c => {
+                    html += "<li onclick=\"detalheCli(c['id_cliente'])\">c['nome']</li>";
+                    
+                });
+                html += "</ul>";
+
+                $('#listaClientes').html(html);
             },
             error: function(){
                 console.log("ERRO!");
             }
         });
     }
+
+//-------------------------------------------------------------------------------
+    function detalheCli(id_cliente){
+        console.log(id_cliente);
+    }
 </script>
-    
+    <!--  -->
 </body>
 </html>
